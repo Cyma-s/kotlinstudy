4f74d9144ef1c10465bf468b5c7320d603d9d2af
Cyma-s <ssol0319@naver.com>
2022-05-08 16:26:17 +0900

    feature: solve 2908

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,9 @@
-fun main(args: Array<String>) {
-    println("Hello World!")
+import java.util.*
 
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+fun main(){
+    val sc = Scanner(System.`in`)
+    val a:Int = sc.next().reversed().toInt()
+    val b:Int = sc.next().reversed().toInt()
+    if (a > b) println(a)
+    else println(b)
 }
